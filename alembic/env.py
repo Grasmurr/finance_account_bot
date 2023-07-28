9de9ae8c5e045a808bbe8647df8d33decf1bec36
Your Name <you@example.com>
2023-07-28 22:34:15 +0300

    changed alembic env.py to correctly use migrations

--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy import MetaData
 
 from alembic import context
 
@@ -16,7 +17,9 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
+from repository import models
 from database import Base
+
 target_metadata = Base.metadata
 
 
--- repository/__init__.py
+++ repository/__init__.py
@@ -1,3 +1,5 @@
-from .users import User
 from .category import Category
+from .users import User
 from .spendings import Spending
+
+models = [User, Spending, Category]
